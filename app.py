1c6c9c4b14eb71b53e9aa2ac2ab91fc6424e77e9
olaolaoni <rayjat51@gmail.com>
2021-04-30 13:57:08 +0100

    added 2 location

--- app.py
+++ app.py
@@ -1,32 +1,35 @@
 import streamlit as st
-import pandas as pd
+import alausa
+import magodo
 
-st.title("The site is under Upgrade...")
-st.text("Uploading Changes in a Bit")
+# st.title("The site is under Upgrade...")
 
-# st.title("Weather Report Dashboard For Alausa")
+st.title('Weather Report Dashboard')
 
-# st.markdown("The dashboard will visualize the correlation between The Constructed Instrument weather data and Standard Instrument Weather data")
+st.markdown("The dashboard will visualize the correlation between The Constructed Instrument weather data and Standard Instrument Weather data")
 
-# def load_data():
-#     data=pd.read_excel("data.xlsx")
-#     return data
+# Sidebar Navigation
+st.sidebar.title('Navigation')
+options = st.sidebar.radio('Select across 10 locations:', 
+    ['ALAUSA', 'MAGODO', 'MAJIDUN', 'OJOKORO IKORODU', 'OLAKETU IKORODU', 'MAYA', 'ISIWU', 'IMOTA IKORODU', 'ODOGIYAN IKORODU', 'IJEDE'])
 
-# data=load_data()
-
-# #st.write(covid_data)
-# st.table(data)
-
-# db_data = data[['SS', 'CS']]
-# temp_data = data[['ST', 'CT']]
-# rh_data = data[['SRH', 'CRH']]
-# pbar_data = data[['SP', 'CP']]
-
-# st.write("S [db] data for both the Constructed and Standard Instrument")
-# st.line_chart(db_data)
-# st.write("Temperature data for both the Constructed and Standard Instrument")
-# st.line_chart(temp_data)
-# st.write("RH [%] data for both the Constructed and Standard Instrument")
-# st.line_chart(rh_data)
-# st.write(" P [mbar] data for both the Constructed and Standard Instrument")
-# st.line_chart(pbar_data)
+if options == 'ALAUSA':
+    alausa.home()
+elif options == 'MAGODO':
+    magodo.home()
+elif options == 'MAJIDUN':
+    raw_data.raw_data(las_file, well_data)
+elif options == 'OJOKORO IKORODU':
+    plotting.plot(las_file, well_data)
+elif options == 'OLAKETU IKORODU':
+    home.home()
+elif options == 'MAYA':
+    home.home()
+elif options == 'ISIWU':
+    home.home()
+elif options == 'IMOTA IKORODU':
+    home.home()
+elif options == 'ODOGIYAN IKORODU':
+    home.home()
+elif options == 'IJEDE':
+    home.home()
---
+++ magodo.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+
+def load_data():
+    data=pd.read_excel("magodo.xlsx")
+    return data
+
+def home():
+    data=load_data()
+
+    st.write('## Report for Magodo')
+    st.table(data)
+
+    db_data = data[['SS', 'CS']]
+    temp_data = data[['ST', 'CT']]
+    rh_data = data[['SRH', 'CRH']]
+    pbar_data = data[['SP', 'CP']]
+
+    max_ss, max_cs = db_data.max()
+    min_ss, min_cs = db_data.min()
+    max_st, max_ct = temp_data.max()
+    # st.text(max_ss)
+    # st.text(max_cs)
+    # st.text(min_ss)
+    # st.text(min_cs)
+
+    st.write(" S [db] data for both the Constructed and Standard Instrument")
+    st.line_chart(db_data)
+    st.write("Temperature data for both the Constructed and Standard Instrument")
+    st.line_chart(temp_data)
+    st.write("RH [%] data for both the Constructed and Standard Instrument")
+    st.line_chart(rh_data)
+    st.write(" P [mbar] data for both the Constructed and Standard Instrument")
+    st.line_chart(pbar_data)
